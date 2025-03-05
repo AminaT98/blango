b7a21375515abbe2957a19d26527d9a18952789f
Aminetou Tekrour <amine.tekrour@gmail.com>
2025-03-05 05:10:34 +0000

    Finish django registration

--- blango_auth/views.py
+++ blango_auth/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+
+@login_required
+def profile(request):
+    return render(request, "blango_auth/profile.html")
 
 # Create your views here.
