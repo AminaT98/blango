3224dd818aa1bb28c98c411ab486a7e0aea58da6
Aminetou Tekrour <amine.tekrour@gmail.com>
2025-03-29 03:03:49 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -69,7 +69,8 @@ class Dev(Configuration):
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
         'rest_framework',
-        'rest_framework.authtoken'
+        'rest_framework.authtoken',
+        'drf_yasg'
     ]
 
     MIDDLEWARE = [
@@ -140,6 +141,13 @@ class Dev(Configuration):
         "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
         ],
+    }
+
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
     }
 
     # Internationalization
